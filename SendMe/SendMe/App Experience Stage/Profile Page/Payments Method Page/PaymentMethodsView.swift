730cc41125b24af24744fe2ce397f020dbd4bc03
Sunny <116433775+SunnyDoe@users.noreply.github.com>
2025-01-14 15:13:25 +0400

    refactor: separated CardView from payments methods

--- SendMe/SendMe/App Experience Stage/Profile Page/Payments Method Page/PaymentMethodsView.swift
+++ SendMe/SendMe/App Experience Stage/Profile Page/Payments Method Page/PaymentMethodsView.swift
@@ -76,31 +76,6 @@ struct PaymentMethodsView: View {
         }
     }
 }
-
-struct CardView: View {
-    let card: SavedCard
-    
-    var body: some View {
-        HStack {
-            card.brand.logo
-                .resizable()
-                .aspectRatio(contentMode: .fit)
-                .frame(width: 40, height: 25)
-            
-            VStack(alignment: .leading) {
-                Text("•••• \(card.lastFourDigits)")
-                    .font(.subheadline)
-                Text(card.expiryDate)
-                    .font(.caption)
-                    .foregroundColor(.gray)
-            }
-            
-            Spacer()
-        }
-        .padding(.vertical, 8)
-    }
-}
-
 extension Notification.Name {
     static let cardAdded = Notification.Name("cardAdded")
 }
