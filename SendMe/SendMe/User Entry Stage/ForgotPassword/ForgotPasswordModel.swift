1bd6fb9aef02a449246b683d098e2437230f1b90
Sunny <116433775+SunnyDoe@users.noreply.github.com>
2025-01-25 16:36:29 +0400

    remove: remove redundent files

--- SendMe/SendMe/User Entry Stage/ForgotPassword/ForgotPasswordModel.swift
+++
@@ -1,9 +0,0 @@
-import Foundation
-
-struct ForgotPasswordModel {
-    var isLoading: Bool
-    var error: String?
-    var isSuccess: Bool
-    
-    static let initial = ForgotPasswordModel(isLoading: false, error: nil, isSuccess: false)
-} 
