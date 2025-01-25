f2449f42facb351106a4ca2e1128e0c612877efd
Sunny <116433775+SunnyDoe@users.noreply.github.com>
2025-01-25 19:44:37 +0400

    feat: added effect for rating view

--- SendMe/SendMe/App Experience Stage/Profile Page/Rate Us Page/RatingView.swift
+++ SendMe/SendMe/App Experience Stage/Profile Page/Rate Us Page/RatingView.swift
@@ -18,6 +18,7 @@ struct RatingView: View {
                         .resizable()
                         .frame(width: 40, height: 40)
                         .foregroundColor(index <= selectedRating ? .yellow : .gray)
+                        .symbolEffect(.bounce, options: .repeat(index <= selectedRating ? 1: 0), value: selectedRating)
                         .onTapGesture {
                             selectedRating = index
                         }
