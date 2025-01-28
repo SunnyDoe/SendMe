170b48c27031b2dd5e47c80e6cce28d33445f69d
Sunny <116433775+SunnyDoe@users.noreply.github.com>
2025-01-28 10:32:48 +0400

    refactor: removed unique handler

--- SendMe/SendMe/App Experience Stage/Profile Page/ProfileModel.swift
+++ SendMe/SendMe/App Experience Stage/Profile Page/ProfileModel.swift
@@ -2,34 +2,15 @@ import UIKit
 
 struct ProfileModel {
     var username: String
-    var handle: String
     var avatar: UIImage?
     var isLoading: Bool
     var error: String?
-    var selectedTab: ProfileTab
-    
-    enum ProfileTab {
-        case activity
-        case replies
-        case media
-    }
-    
-    static func generateUniqueHandle() -> String {
-        String(format: "%06d", Int.random(in: 100000...999999))
-    }
     
     static let initial = ProfileModel(
         username: "Your Name",
-        handle: UserDefaults.standard.string(forKey: "userHandle") ?? generateUniqueHandle(),
         avatar: UserDefaults.standard.savedAvatar,
         isLoading: false,
-        error: nil,
-        selectedTab: .activity
+        error: nil
     )
 }
 
-extension UserDefaults {
-    func saveHandle(_ handle: String) {
-        set(handle, forKey: "userHandle")
-    }
-} 
